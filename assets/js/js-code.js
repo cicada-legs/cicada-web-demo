0e72101ab22a143e55a88c59f3ce96b13a2c2aca
Bobo <wooorkbo@gmail.com>
2022-09-07 13:56:57 +1200

    working weekday display

--- assets/js/js-code.js
+++ assets/js/js-code.js
@@ -1,5 +1,6 @@
-<script>
-    var day = new Date();
-    var time;
-
-</script>
+function displayTime(){
+    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date();
+    let today = dayNames[date.getDay()];
+    document.getElementById('time').innerHTML = today;
+}
